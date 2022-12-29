b14994c66e17302f566b1768ac7b553a0b393fb9
Arik Chakma <arikchangma@gmail.com>
2022-12-29 23:20:29 +0600

    feat: add invite API route
    This commit adds the invite API route.
    
    The route is not yet implemented.

--- pages/api/project/invite/index.ts
+++ pages/api/project/invite/index.ts
@@ -1,13 +1,19 @@
 // Next.js API route support: https://nextjs.org/docs/api-routes/introduction
 import type { NextApiRequest, NextApiResponse } from 'next';
 
-type Data = {
-	name: string;
-};
-
-export default function handler(
-	req: NextApiRequest,
-	res: NextApiResponse<Data>
-) {
-	res.status(200).json({ name: 'John Doe' });
+export default function handler(req: NextApiRequest, res: NextApiResponse) {
+	if (req.method === 'POST') {
+		/**
+		 * POST: /api/projects/[slug]/invite – invite a teammate
+		 * TODO: 1. Check if the user is authenticated
+		 * TODO: 2. Check if the user is the owner of the project
+		 * TODO: 3. Check if the user is already a member of the project
+		 * TODO: 4. Check if the user is already invited to the project
+		 * TODO: 5. Create a token
+		 * TODO: 6. Hash the token
+		 * TODO: 7. Save the token to the database
+		 * TODO: 8. Send an email to the user
+		 * TODO: 9. Return a success message
+		 */
+	}
 }
