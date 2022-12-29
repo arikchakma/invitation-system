7bc5b3efb379bc378783138f4a9cbe7dac7ca253
Arik Chakma <arikchangma@gmail.com>
2022-12-30 00:46:06 +0600

    feat: add project slug
    Add a slug to projects to make them easier to reference.
    
    This is a breaking change, as it requires a migration.

---
+++ lib/auth.ts
@@ -0,0 +1,77 @@
+import { NextApiRequest, NextApiResponse } from 'next';
+import { Session, unstable_getServerSession } from 'next-auth';
+import { authOptions } from '@/pages/api/auth/[...nextauth]';
+import prisma from '@/lib/prisma';
+
+export async function getServerSession(
+	req: NextApiRequest,
+	res: NextApiResponse
+) {
+	return (await unstable_getServerSession(req, res, authOptions)) as Session;
+}
+
+interface WithProjectAuth {
+	(req: NextApiRequest, res: NextApiResponse): Promise<void>;
+}
+
+const withProjectAuth =
+	(handler: WithProjectAuth) =>
+	async (req: NextApiRequest, res: NextApiResponse) => {
+		const session = await getServerSession(req, res);
+		if (!session?.user?.id) return res.status(401).end('Unauthorized');
+
+		const { slug } = req.query;
+		if (!slug || typeof slug !== 'string') {
+			return res
+				.status(400)
+				.json({ error: 'Missing or misconfigured project slug' });
+		}
+
+		const project = await prisma.project.findUnique({
+			where: {
+				slug,
+			},
+			select: {
+				id: true,
+				name: true,
+				slug: true,
+				users: {
+					where: {
+						userId: session.user.id,
+					},
+					select: {
+						role: true,
+					},
+				},
+			},
+		});
+
+		if (project) {
+			// project exists but user is not part of it
+			if (project.users.length === 0) {
+				const pendingInvites = await prisma.projectInvite.findUnique({
+					where: {
+						email_projectId: {
+							email: String(session?.user?.email),
+							projectId: project.id,
+						},
+					},
+					select: {
+						expires: true,
+					},
+				});
+				if (!pendingInvites) {
+					return res.status(404).json({ error: 'Project not found' });
+				} else if (pendingInvites.expires < new Date()) {
+					return res.status(410).json({ error: 'Project invite expired' });
+				} else {
+					return res.status(409).json({ error: 'Project invite pending' });
+				}
+			}
+		} else {
+			// project doesn't exist
+			return res.status(404).json({ error: 'Project not found' });
+		}
+
+		return handler(req, res);
+	};
