1f67a9d574e9c95fc3ee2d341d9a9881d5decdf9
Arik Chakma <arikchangma@gmail.com>
2022-12-31 01:18:54 +0600

    feat: Add project creation
    - Check if the user already has a project with the same name
    - Create a project
    - Return a success message

--- pages/api/project/index.ts
+++ pages/api/project/index.ts
@@ -39,20 +39,17 @@ export default withUserAuth(
 			return res.status(200).json(projects);
 		} else if (req.method === 'POST') {
 			/**
-			 * TODO: 1. Check if the user already has a project with the same name
-			 * TODO: 2. Create a slug
+			 * TODO: 1. Check validation of the name and slug
+			 * TODO: 2. Check if the user already has a project with the same name
 			 * TODO: 3. Create a project
-			 * TODO: 4. Add the user as a member of the project
-			 * TODO: 5. Return the project
-			 * TODO: 6. Handle errors
-			 * TODO: 7. Return a success message
+			 * TODO: 4. Return a success message
 			 */
 			const { name, slug } = req.body as {
 				name: string;
 				slug: string;
 			};
 
-			// Guard clause to check if the name and slug are valid
+			// 1. Guard clause to check if the name and slug are valid
 			if (!name || !slug)
 				return res.status(400).json({ error: 'Invalid project name or slug' });
 
@@ -60,7 +57,7 @@ export default withUserAuth(
 			if (!/^[a-z0-9-]+$/.test(slug) || slug.includes(' '))
 				return res.status(400).json({ error: 'Invalid project slug' });
 
-			// 1. Check if the user already has a project with the same name
+			// 2. Check if the user already has a project with the same name
 			const existingProject = await prisma.project.findFirst({
 				where: {
 					slug,
@@ -76,6 +73,38 @@ export default withUserAuth(
 				return res
 					.status(400)
 					.json({ error: 'Project with this name already exists' });
+
+			// 3. Create a project
+			const project = await prisma.project.create({
+				data: {
+					name,
+					slug,
+					users: {
+						create: {
+							role: 'owner',
+							user: {
+								connect: {
+									id: user?.id,
+								},
+							},
+						},
+					},
+				},
+				select: {
+					id: true,
+					name: true,
+					slug: true,
+					users: {
+						select: {
+							role: true,
+							createdAt: true,
+						},
+					},
+				},
+			});
+
+			// 4. Return a success message
+			return res.status(201).json(project);
 		}
 	},
 	{
