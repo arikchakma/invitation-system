cb02a86f25542f03e1eaf7951a41ffe7ad79478b
Arik Chakma <arikchangma@gmail.com>
2022-12-30 14:57:58 +0600

    feat: Add project invite API
    This adds a new API endpoint to get all pending invitations for a project.
    
    - GET /api/projects/[slug]/invite
    
    This endpoint returns an array of objects with the following properties:
    
    - email: The email address of the invited user
    - joinedAt: The date the invitation was sent

--- pages/api/project/invite/index.ts
+++ pages/api/project/invite/index.ts
@@ -1,24 +1,37 @@
 // Next.js API route support: https://nextjs.org/docs/api-routes/introduction
 import { withProjectAuth } from '@/lib/auth';
 import type { NextApiRequest, NextApiResponse } from 'next';
+import prisma from '@/lib/prisma';
 
 export default withProjectAuth(
-	async (req: NextApiRequest, res: NextApiResponse) => {
+	async (req: NextApiRequest, res: NextApiResponse, project) => {
+		const { slug } = req.query;
+
+		// Check if the slug is valid
+		if (!slug || typeof slug !== 'string')
+			return res.status(400).json({ error: 'Invalid project slug' });
+
 		if (req.method === 'GET') {
 			/**
 			 * GET: /api/projects/[slug]/invite – get all pending invitations
-			 * TODO: 1. Check if the user is authenticated
-			 * TODO: 2. Check if the user is the owner of the project
-			 * TODO: 3. Get all pending invitations for the project
-			 * TODO: 4. Return the invitations
+			 * TODO: 1. Get all pending invitations for the project
+			 * TODO: 2. Return the invitations
 			 */
-			const { slug } = req.query;
-
-			// Check if the slug is valid
-			if (!slug || typeof slug !== 'string')
-				return res.status(400).json({ error: 'Invalid project slug' });
-
-			// 1. Check if the user is authenticated
+			const invites = await prisma.projectInvite.findMany({
+				where: {
+					projectId: project?.id,
+				},
+				select: {
+					email: true,
+					createdAt: true,
+				},
+			});
+			return res.status(200).json(
+				invites.map((invite) => ({
+					email: invite.email,
+					joinedAt: invite.createdAt,
+				}))
+			);
 		} else if (req.method === 'POST') {
 			/**
 			 * POST: /api/projects/[slug]/invite – invite a teammate
