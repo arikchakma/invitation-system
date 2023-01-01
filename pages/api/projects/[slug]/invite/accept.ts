0dec0ac471d9e5035e0efc4ea51544faa366b936
Arik Chakma <arikchangma@gmail.com>
2023-01-02 04:46:45 +0600

    feat: add project invite accept API
    This API allows users to accept project invites.

--- pages/api/projects/[slug]/invite/accept.ts
+++ pages/api/projects/[slug]/invite/accept.ts
@@ -1,13 +1,52 @@
-// Next.js API route support: https://nextjs.org/docs/api-routes/introduction
-import type { NextApiRequest, NextApiResponse } from 'next';
+import { withUserAuth } from '@/lib/auth';
+import prisma from '@/lib/prisma';
+import { NextApiRequest, NextApiResponse } from 'next';
 
-type Data = {
-	name: string;
-};
-
-export default function handler(
-	req: NextApiRequest,
-	res: NextApiResponse<Data>
-) {
-	res.status(200).json({ name: 'John Doe' });
-}
+export default withUserAuth(
+	async (req: NextApiRequest, res: NextApiResponse, session) => {
+		const { slug } = req.query as { slug: string };
+		if (!slug || typeof slug !== 'string') {
+			return res
+				.status(400)
+				.json({ error: 'Missing or misconfigured project slug' });
+		}
+		if (req.method === 'POST') {
+			const invite = await prisma.projectInvite.findFirst({
+				where: {
+					email: String(session?.user?.email),
+					project: {
+						slug,
+					},
+				},
+				select: {
+					expires: true,
+					projectId: true,
+				},
+			});
+			if (!invite) {
+				return res.status(404).json({ error: 'Invalid invitation' });
+			} else if (invite.expires < new Date()) {
+				return res.status(410).json({ error: 'Invitation expired' });
+			} else {
+				const response = await Promise.all([
+					prisma.projectUser.create({
+						data: {
+							userId: String(session?.user?.id),
+							role: 'member',
+							projectId: invite.projectId,
+						},
+					}),
+					prisma.projectInvite.delete({
+						where: {
+							email_projectId: {
+								email: String(session?.user?.email),
+								projectId: invite.projectId,
+							},
+						},
+					}),
+				]);
+				return res.status(200).json(response);
+			}
+		}
+	}
+);
