4f587bf63358cf558e9b307299e71e179825e787
Arik Chakma <arikchangma@gmail.com>
2023-01-01 06:26:00 +0600

    feat: add projects page
    - Add projects page
    - Add projects API endpoint
    - Add projects query

--- pages/projects.tsx
+++ pages/projects.tsx
@@ -1,10 +1,19 @@
+import { useQuery } from '@tanstack/react-query';
 import { useSession } from 'next-auth/react';
 
 export default function Projects() {
 	const { data: session } = useSession();
+	const { data: projects } = useQuery(['projects'], async () => {
+		const res = await fetch('/api/projects');
+		return res.json();
+	});
 
 	if (!session) {
 		return <div>Not signed in</div>;
+	}
+
+	if (projects?.length === 0) {
+		return <div>No projects found</div>;
 	}
 
 	return <div>Projects</div>;
