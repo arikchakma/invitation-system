b9c6369f99bf968e007dd5a2f80090d8c1e5479e
Arik Chakma <arikchangma@gmail.com>
2023-01-01 22:15:50 +0600

    feat: add project page
    - Add project page
    - Add project API

--- pages/[slug]/index.tsx
+++ pages/[slug]/index.tsx
@@ -1,3 +1,23 @@
-export default function Project() {
+import { Project } from '@prisma/client';
+import { useQuery } from '@tanstack/react-query';
+import { useRouter } from 'next/router';
+
+export default function ProjectPage() {
+	const router = useRouter();
+	const { slug } = router.query as {
+		slug: string;
+	};
+	const { data: project } = useQuery<Project>(
+		['project', slug],
+		async () => {
+			return (await fetch(`/api/projects/${slug}`)).json();
+		},
+		{
+			enabled: !!slug,
+		}
+	);
+
+	console.log(router.query, project);
+
 	return <div>index</div>;
 }
--- pages/api/projects/[slug]/index.ts
+++ pages/api/projects/[slug]/index.ts
@@ -1,9 +1,28 @@
 // Next.js API route support: https://nextjs.org/docs/api-routes/introduction
-import { withUserAuth } from '@/lib/auth';
+import { withProjectAuth } from '@/lib/auth';
 import type { NextApiRequest, NextApiResponse } from 'next';
+import prisma from '@/lib/prisma';
 
-export default withUserAuth(
-	async (req: NextApiRequest, res: NextApiResponse) => {
-		res.status(200).json({ name: req.query });
+export default withProjectAuth(
+	async (req: NextApiRequest, res: NextApiResponse, project) => {
+		if (req.method === 'GET') {
+			// GET /api/projects/[slug]
+			res.status(200).json(project);
+		} else if (req.method === 'PUT') {
+			// PUT /api/projects/[slug]
+			const { name } = req.body;
+			const updatedProject = await prisma.project.update({
+				where: {
+					id: project?.id,
+				},
+				data: {
+					name,
+				},
+			});
+			res.status(200).json(updatedProject);
+		} else {
+			res.setHeader('Allow', ['GET', 'PUT']);
+			res.status(405).end(`Method ${req.method} Not Allowed`);
+		}
 	}
 );
