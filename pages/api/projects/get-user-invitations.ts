f67289b067ab1504fb5caaf070557b7858b91126
Arik Chakma <arikchangma@gmail.com>
2023-01-02 17:05:40 +0600

    feat: add pending invitations to the dashboard
    This adds the pending invitations to the dashboard.
    
    The user can now see all the pending invitations and accept or decline them.

--- pages/api/projects/get-user-invitations.ts
+++ pages/api/projects/get-user-invitations.ts
@@ -1,33 +1,32 @@
 import { withUserAuth } from '@/lib/auth';
 import prisma from '@/lib/prisma';
 
-export default withUserAuth(
-	async (req, res, session, user) => {
-		if (req.method === 'GET') {
-			/**
-			 * GET: /api/projects/[slug]/invite – get all pending invitations
-			 * TODO: 1. Get all pending invitations for the project
-			 * TODO: 2. Return the invitations
-			 */
-			const invites = await prisma.projectInvite.findMany({
-				where: {
-					email: user?.email,
+export default withUserAuth(async (req, res, session) => {
+	if (req.method === 'GET') {
+		/**
+		 * GET: /api/projects/[slug]/invite – get all pending invitations
+		 * TODO: 1. Get all pending invitations for the project
+		 * TODO: 2. Return the invitations
+		 */
+		const invites = await prisma.projectInvite.findMany({
+			where: {
+				email: String(session?.user?.email),
+			},
+			select: {
+				email: true,
+				createdAt: true,
+				project: {
+					select: {
+						name: true,
+						slug: true,
+					},
 				},
-				select: {
-					email: true,
-					createdAt: true,
-				},
-			});
+			},
+		});
 
-			res.status(200).json(invites);
-		} else {
-			res.setHeader('Allow', ['GET']);
-			return res
-				.status(405)
-				.json({ error: `Method ${req.method} Not Allowed` });
-		}
-	},
-	{
-		needUserDetails: true,
+		res.status(200).json(invites);
+	} else {
+		res.setHeader('Allow', ['GET']);
+		return res.status(405).json({ error: `Method ${req.method} Not Allowed` });
 	}
-);
+});
--- pages/index.tsx
+++ pages/index.tsx
@@ -1,24 +1,36 @@
 import { Inter } from '@next/font/google';
-import { useEffect, useState } from 'react';
-import { signIn, useSession } from 'next-auth/react';
-import { useRouter } from 'next/router';
 import { useQuery } from '@tanstack/react-query';
+import { timeAgo } from '@/lib/utils';
 
 const inter = Inter({ subsets: ['latin'] });
 
 export default function Home() {
-	const { data: pendingInivatations } = useQuery(
-		['pendingInvitations'],
-		async () => {
-			return (await fetch('/api/projects/get-user-invitations')).json();
-		}
-	);
+	const { data: pendingInivatations } = useQuery<
+		{
+			email: string;
+			createdAt: Date;
+			project: {
+				name: string;
+				slug: string;
+			};
+		}[]
+	>(['pendingInvitations'], async () => {
+		return (await fetch('/api/projects/get-user-invitations')).json();
+	});
 
 	console.log(pendingInivatations);
 
 	return (
 		<main className="flex items-center justify-center px-5 h-screen">
-			Hello
+			<ul>
+				{pendingInivatations?.map((invitation, index) => (
+					<li key={invitation.email + index}>
+						<h1>{invitation.email}</h1>
+						<h2>{invitation.project.name}</h2>
+						<p>Invited {timeAgo(invitation.createdAt)}</p>
+					</li>
+				))}
+			</ul>
 		</main>
 	);
 }
