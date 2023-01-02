434747ace424eaacaeab3fb0ac7c9b55a79a6c80
Arik Chakma <arikchangma@gmail.com>
2023-01-02 20:03:52 +0600

    feat: Add pending invitations page
    This page will show all pending invitations for the current user.
    
    The user can accept the invitation and will be redirected to the project page.

--- pages/index.tsx
+++ pages/index.tsx
@@ -20,7 +20,6 @@ export default function Home() {
 	>(['pendingInvitations'], async () => {
 		return (await fetch('/api/projects/get-user-invitations')).json();
 	});
-	const session = useSession();
 
 	const acceptInvitation = useMutation(async (slug: string) => {
 		return await fetch(`/api/projects/${slug}/invite/accept`, {
@@ -29,30 +28,38 @@ export default function Home() {
 		});
 	});
 
-	console.log(pendingInivatations, session);
-
 	return (
-		<main className="flex items-center justify-center px-5 h-screen">
-			<ul>
+		<main className="p-10">
+			<ul className="max-w-xl">
 				{pendingInivatations?.map((invitation, index) => (
 					<li key={invitation.email + index}>
-						<h1>{invitation.email}</h1>
-						<h2>{invitation.project.name}</h2>
-						<p>Invited {timeAgo(invitation.createdAt)}</p>
-						<button
-							onClick={() => {
-								acceptInvitation.mutate(invitation.project.slug, {
-									onSuccess: () => {
-										router.push(`/${invitation.project.slug}`);
-									},
-								});
-							}}
-						>
-							Accept
-						</button>
+						<div>
+							<p>
+								<strong>{invitation.project.name}</strong> has invited you{' '}
+								<strong>{timeAgo(invitation.createdAt)}</strong> to join their
+								project. By accepting this invitation you will be able to view
+								and edit the project.
+							</p>
+							<button
+								onClick={() => {
+									acceptInvitation.mutate(invitation.project.slug, {
+										onSuccess: () => {
+											router.push(`/${invitation.project.slug}`);
+										},
+									});
+								}}
+								className="bg-black text-white px-4 py-1 mt-2 rounded"
+							>
+								Accept
+							</button>
+						</div>
 					</li>
 				))}
 			</ul>
+
+			<div>
+				{pendingInivatations?.length === 0 && <p>No pending invitations</p>}
+			</div>
 		</main>
 	);
 }
