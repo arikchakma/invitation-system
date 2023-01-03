aa634b2bd913b2691a66ccdd6dd0f32be3b5cfd4
Arik Chakma <arikchangma@gmail.com>
2023-01-03 21:19:21 +0600

    feat: add invite user form
    This form allows the project owner to invite a user to the project.

---
+++ components/projects/invite-user-form.tsx
@@ -0,0 +1,55 @@
+import useProject from '@/utils/use-project';
+import { useMutation, useQueryClient } from '@tanstack/react-query';
+import { useRouter } from 'next/router';
+import { useForm } from 'react-hook-form';
+
+export default function InviteUserForm() {
+	const router = useRouter();
+	const { slug } = router.query as {
+		slug: string;
+	};
+	const utils = useQueryClient();
+	const { register, handleSubmit } = useForm();
+	const { project } = useProject();
+
+	const inviteUser = useMutation({
+		mutationFn: async (data: { email: string }) => {
+			return (
+				await fetch(`/api/projects/${slug}/invite`, {
+					method: 'POST',
+					body: JSON.stringify(data),
+				})
+			).json();
+		},
+		onSuccess: (data) => {
+			utils.invalidateQueries(['invitations', slug]);
+		},
+	});
+
+	const onSubmit = handleSubmit((data) => {
+		inviteUser.mutate(data as { email: string }, {
+			onSuccess: (data) => {
+				console.log(data);
+			},
+		});
+	});
+	return (
+		<form onSubmit={onSubmit} className="mt-10">
+			<label>
+				<span>Invite a user to {project?.name}</span>
+				<input
+					type="text"
+					placeholder="Email"
+					className="mt-1 block w-full appearance-none rounded-md border border-gray-300 px-3 py-2 placeholder-gray-400 shadow-sm focus:border-black focus:outline-none focus:ring-black sm:text-sm"
+					{...register('email')}
+				/>
+			</label>
+			<button
+				type="submit"
+				className="mt-2 w-full justify-center inline-flex items-center px-4 py-2 border border-transparent shadow-sm text-sm font-medium rounded-md text-white bg-black hover:bg-gray-900 focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-black"
+			>
+				Invite
+			</button>
+		</form>
+	);
+}
--- pages/[slug]/index.tsx
+++ pages/[slug]/index.tsx
@@ -1,56 +1,16 @@
 import UsersTable from '@/components/projects/users-table';
 import InvitationsTable from '@/components/projects/invitations-table';
 import MaxWidthWrapper from '@/layouts/max-width-wrapper';
-import { ProjectUserProps } from '@/types/project';
-import { useMutation, useQuery, useQueryClient } from '@tanstack/react-query';
-import { useSession } from 'next-auth/react';
 import { useRouter } from 'next/router';
-import { useForm } from 'react-hook-form';
 import useProject from '@/utils/use-project';
+import InviteUserForm from '@/components/projects/invite-user-form';
 
 export default function ProjectPage() {
 	const router = useRouter();
-	const utils = useQueryClient();
-	const session = useSession();
-	const { register, handleSubmit } = useForm();
 	const { slug } = router.query as {
 		slug: string;
 	};
-	const { project, isOwner } = useProject();
-
-	const { data: users } = useQuery<ProjectUserProps[]>(
-		['users', slug],
-		async () => {
-			return (await fetch(`/api/projects/${slug}/users`)).json();
-		},
-		{
-			enabled: !!slug,
-		}
-	);
-
-	const inviteUser = useMutation({
-		mutationFn: async (data: { email: string }) => {
-			return (
-				await fetch(`/api/projects/${slug}/invite`, {
-					method: 'POST',
-					body: JSON.stringify(data),
-				})
-			).json();
-		},
-		onSuccess: (data) => {
-			utils.invalidateQueries(['invitations', slug]);
-		},
-	});
-
-	const onSubmit = handleSubmit((data) => {
-		inviteUser.mutate(data as { email: string }, {
-			onSuccess: (data) => {
-				console.log(data);
-			},
-		});
-	});
-
-	console.log(router.query, project, users);
+	const { project } = useProject();
 
 	return (
 		<main className="mt-20">
@@ -59,25 +19,7 @@ export default function ProjectPage() {
 					<h1 className="font-bold text-3xl">{project?.name}</h1>
 					<p>{project?.slug}</p>
 				</div>
-
-				<form onSubmit={onSubmit} className="mt-10">
-					<label>
-						<span>Invite a user to {project?.name}</span>
-						<input
-							type="text"
-							placeholder="Email"
-							className="mt-1 block w-full appearance-none rounded-md border border-gray-300 px-3 py-2 placeholder-gray-400 shadow-sm focus:border-black focus:outline-none focus:ring-black sm:text-sm"
-							{...register('email')}
-						/>
-					</label>
-					<button
-						type="submit"
-						className="mt-2 w-full justify-center inline-flex items-center px-4 py-2 border border-transparent shadow-sm text-sm font-medium rounded-md text-white bg-black hover:bg-gray-900 focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-black"
-					>
-						Invite
-					</button>
-				</form>
-
+				<InviteUserForm />
 				<div className="mt-10">
 					<UsersTable />
 					<InvitationsTable />
