f3857adc337dbd9250761c219e8af359b2488c81
Arik Chakma <arikchangma@gmail.com>
2023-01-05 00:01:59 +0600

    Add link to projects page
    This is a good place to add a link to the projects page.

--- pages/index.tsx
+++ pages/index.tsx
@@ -2,6 +2,7 @@ import { Inter } from '@next/font/google';
 import { useRouter } from 'next/router';
 import { signOut, useSession } from 'next-auth/react';
 import { useEffect } from 'react';
+import NextLink from 'next/link';
 import MaxWidthWrapper from '@/layouts/max-width-wrapper';
 import PendingInvitationsTable from '@/components/projects/pending-invitations-table';
 
@@ -19,14 +20,23 @@ export default function Home() {
 		<main className="mt-20">
 			<MaxWidthWrapper>
 				<h1 className="text-3xl font-bold">{session?.data?.user?.email}</h1>
-				<button
-					className="bg-black text-white px-4 py-1 mt-2 rounded"
-					onClick={() => {
-						signOut();
-					}}
-				>
-					Log Out
-				</button>
+				<div className="flex gap-2">
+					<NextLink
+						href="/projects"
+						className="bg-black inline-block text-white px-4 py-1 mt-2 rounded"
+					>
+						Projects
+					</NextLink>
+					<button
+						className="bg-black text-white px-4 py-1 mt-2 rounded"
+						onClick={() => {
+							signOut();
+						}}
+					>
+						Log Out
+					</button>
+				</div>
+
 				<PendingInvitationsTable />
 			</MaxWidthWrapper>
 		</main>
