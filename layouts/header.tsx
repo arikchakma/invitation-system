e1e55e3a5a5fc30a34683a39ad1b5249637003a2
Arik Chakma <arikchangma@gmail.com>
2023-01-05 17:29:10 +0600

    Add a placeholder for the header when the user is not logged in
    This is a placeholder for the header when the user is not logged in.
    
    It is a div with a height of 9px and a width of 72px.
    
    It has a background color of slate-200 and a border radius of 9px.

--- layouts/header.tsx
+++ layouts/header.tsx
@@ -6,9 +6,13 @@ export default function Header() {
 
 	return (
 		<header className="mb-10">
-			<h1 className="text-3xl font-bold bg-gradient-to-br bg-clip-text text-transparent from-slate-900 to-slate-200">
-				{session?.data?.user?.email}
-			</h1>
+			{session.data ? (
+				<h1 className="text-3xl font-bold bg-gradient-to-br bg-clip-text text-transparent from-slate-900 to-slate-200">
+					{session?.data?.user?.email}
+				</h1>
+			) : (
+				<div className="h-9 rounded bg-slate-200 w-72" />
+			)}
 			<div className="flex gap-2">
 				<NextLink
 					href="/"
