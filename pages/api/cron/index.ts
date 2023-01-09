2a265e4b84ddf0c59054b9d757767db3c8f71f24
Arik Chakma <arikchangma@gmail.com>
2023-01-10 02:35:23 +0600

    feat: add cron job
    This adds a cron job to the API that will be used to update the data.

---
+++ pages/api/cron/index.ts
@@ -0,0 +1,23 @@
+import { NextApiRequest, NextApiResponse } from 'next';
+
+export default async function handler(
+	req: NextApiRequest,
+	res: NextApiResponse
+) {
+	if (req.method === 'POST') {
+		try {
+			const { authorization } = req.headers;
+
+			if (authorization === `Bearer ${process.env.API_SECRET_KEY}`) {
+				res.status(200).json({ success: true });
+			} else {
+				res.status(401).json({ success: false });
+			}
+		} catch (err) {
+			res.status(500).json({ statusCode: 500, message: (err as any).message });
+		}
+	} else {
+		res.setHeader('Allow', 'POST');
+		res.status(405).end(`Method ${req.method} Not Allowed`);
+	}
+}
