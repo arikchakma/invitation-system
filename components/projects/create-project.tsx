0f3b123c818304f500f1e5bb2a76af5feb310c97
Arik Chakma <arikchangma@gmail.com>
2023-01-13 23:10:41 +0600

    prettier

--- components/projects/create-project.tsx
+++ components/projects/create-project.tsx
@@ -3,65 +3,65 @@ import { useMutation, useQueryClient } from '@tanstack/react-query';
 import { useForm } from 'react-hook-form';
 
 export default function CreateProject() {
-	const utils = useQueryClient();
-	const { register, handleSubmit } = useForm<{
-		name: string;
-		slug: string;
-	}>();
+  const utils = useQueryClient();
+  const { register, handleSubmit } = useForm<{
+    name: string;
+    slug: string;
+  }>();
 
-	const createProject = useMutation({
-		mutationFn: async (data: { name: string; slug: string }) => {
-			return (
-				await fetch('/api/projects', {
-					method: 'POST',
-					body: JSON.stringify(data),
-				})
-			).json() as Promise<ProjectProps>;
-		},
-	});
+  const createProject = useMutation({
+    mutationFn: async (data: { name: string; slug: string }) => {
+      return (
+        await fetch('/api/projects', {
+          method: 'POST',
+          body: JSON.stringify(data),
+        })
+      ).json() as Promise<ProjectProps>;
+    },
+  });
 
-	const onSubmit = handleSubmit(async (data) => {
-		const project = await createProject.mutateAsync(data, {
-			onSuccess: async (project) => {
-				console.log(project);
-				utils.invalidateQueries(['projects']);
-			},
-		});
+  const onSubmit = handleSubmit(async data => {
+    const project = await createProject.mutateAsync(data, {
+      onSuccess: async project => {
+        console.log(project);
+        utils.invalidateQueries(['projects']);
+      },
+    });
 
-		console.log(project);
-	});
+    console.log(project);
+  });
 
-	return (
-		<div>
-			<h1 className="text-3xl font-bold">Create Project</h1>
-			<form onSubmit={onSubmit} className="mt-5 flex flex-col gap-2">
-				<label className="block">
-					<span className="text-gray-700">Project Name</span>
-					<input
-						type="text"
-						{...register('name', { required: true })}
-						placeholder="Arik Chakma"
-						className="mt-1 block w-full rounded-md border-gray-300 shadow-sm focus:border-indigo-300 focus:ring focus:ring-indigo-200/50"
-					/>
-				</label>
-				<label>
-					<span className="text-gray-700">Project Slug</span>
-					<div className="mt-1 flex">
-						<span className="inline-flex items-center rounded-l-md border border-r-0 border-gray-300 bg-gray-100 px-4 text-gray-600">
-							wth.is
-						</span>
-						<input
-							type="text"
-							{...register('slug', { required: true })}
-							placeholder="arik-chakma"
-							className="block w-full rounded-r-md border-gray-300 shadow-sm focus:border-indigo-300 focus:ring focus:ring-indigo-200/50"
-						/>
-					</div>
-				</label>
-				<button type="submit" className="rounded-md bg-black p-2 text-white">
-					Create project
-				</button>
-			</form>
-		</div>
-	);
+  return (
+    <div>
+      <h1 className="text-3xl font-bold">Create Project</h1>
+      <form onSubmit={onSubmit} className="mt-5 flex flex-col gap-2">
+        <label className="block">
+          <span className="text-gray-700">Project Name</span>
+          <input
+            type="text"
+            {...register('name', { required: true })}
+            placeholder="Arik Chakma"
+            className="mt-1 block w-full rounded-md border-gray-300 shadow-sm focus:border-indigo-300 focus:ring focus:ring-indigo-200/50"
+          />
+        </label>
+        <label>
+          <span className="text-gray-700">Project Slug</span>
+          <div className="mt-1 flex">
+            <span className="inline-flex items-center rounded-l-md border border-r-0 border-gray-300 bg-gray-100 px-4 text-gray-600">
+              wth.is
+            </span>
+            <input
+              type="text"
+              {...register('slug', { required: true })}
+              placeholder="arik-chakma"
+              className="block w-full rounded-r-md border-gray-300 shadow-sm focus:border-indigo-300 focus:ring focus:ring-indigo-200/50"
+            />
+          </div>
+        </label>
+        <button type="submit" className="rounded-md bg-black p-2 text-white">
+          Create project
+        </button>
+      </form>
+    </div>
+  );
 }
--- pages/projects.tsx
+++ pages/projects.tsx
@@ -1,43 +1,43 @@
-import { Project } from '@prisma/client';
-import { useQuery, useQueryClient } from '@tanstack/react-query';
-import { useSession } from 'next-auth/react';
 import NextLink from 'next/link';
-import MaxWidthWrapper from '@/layouts/max-width-wrapper';
+import { Project } from '@prisma/client';
+import { useQuery } from '@tanstack/react-query';
+import { useSession } from 'next-auth/react';
 import CreateProject from '@/components/projects/create-project';
 import Container from '@/layouts/container';
+import MaxWidthWrapper from '@/layouts/max-width-wrapper';
 import { QueryError, fetcher } from '@/utils/fetcher';
 
 export default function Projects() {
-	const { data: session } = useSession();
+  const { data: session } = useSession();
 
-	const { data: projects, isSuccess } = useQuery<Project[], QueryError>(
-		['projects'],
-		async () => fetcher('/api/projects')
-	);
+  const { data: projects, isSuccess } = useQuery<Project[], QueryError>(
+    ['projects'],
+    async () => fetcher('/api/projects')
+  );
 
-	if (!session) {
-		return <div>Not signed in</div>;
-	}
+  if (!session) {
+    return <div>Not signed in</div>;
+  }
 
-	return (
-		<Container>
-			<MaxWidthWrapper>
-				<CreateProject />
+  return (
+    <Container>
+      <MaxWidthWrapper>
+        <CreateProject />
 
-				<div className="mt-10">
-					<h2 className="text-2xl font-bold">Projects</h2>
-					<div className="mt-5 grid grid-cols-4 gap-5">
-						{isSuccess &&
-							projects?.map((project) => (
-								<NextLink href={`/${project.slug}`} key={project.id}>
-									<div className="rounded-md border border-gray-100/40 bg-white p-5 shadow transition hover:shadow-md">
-										<p>{project.name}</p>
-									</div>
-								</NextLink>
-							))}
-					</div>
-				</div>
-			</MaxWidthWrapper>
-		</Container>
-	);
+        <div className="mt-10">
+          <h2 className="text-2xl font-bold">Projects</h2>
+          <div className="mt-5 grid grid-cols-4 gap-5">
+            {isSuccess &&
+              projects?.map(project => (
+                <NextLink href={`/${project.slug}`} key={project.id}>
+                  <div className="rounded-md border border-gray-100/40 bg-white p-5 shadow transition hover:shadow-md">
+                    <p>{project.name}</p>
+                  </div>
+                </NextLink>
+              ))}
+          </div>
+        </div>
+      </MaxWidthWrapper>
+    </Container>
+  );
 }
