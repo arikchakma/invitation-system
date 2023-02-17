bc8de00a3c58f36c1676a3522e14ac6a362ffc8f
Arik Chakma <arikchangma@gmail.com>
2023-02-18 04:15:20 +0600

    Add a className to the chat overlay
    This className is used to add a red border to the chat overlay when the user
    scrolls to the top or bottom of the chat.

--- components/projects/chatting.tsx
+++ components/projects/chatting.tsx
@@ -1,4 +1,5 @@
 import { useEffect, useRef, useState } from 'react';
+import cn from 'clsx';
 import {
   PusherProvider,
   useCurrentMemberCount,
@@ -9,6 +10,10 @@ import useProject from '@/utils/use-project';
 function Chat() {
   const ref = useRef<HTMLDivElement>(null);
   const { project } = useProject();
+  const [overlay, setOverlay] = useState({
+    top: false,
+    bottom: false,
+  });
   const [messages, setMessages] = useState<
     {
       sender: string;
@@ -24,27 +29,73 @@ function Chat() {
     ref.current?.scrollTo(0, ref.current.scrollHeight);
   }, [messages]);
 
+  useEffect(() => {
+    const target = ref.current;
+    if (!target) return;
+
+    if (target.scrollHeight > target.clientHeight)
+      setOverlay(prev => ({ ...prev, top: true }));
+
+    console.log(target.scrollHeight, target.clientHeight);
+    const scroll = (e: Event) => {
+      if (target.scrollTop === 0) {
+        console.log('Scrolled to top');
+        setOverlay(prev => ({ top: false, bottom: true }));
+      } else if (
+        target.scrollTop + target.clientHeight ===
+        target.scrollHeight
+      ) {
+        console.log('Scrolled to bottom');
+        setOverlay(prev => ({ top: true, bottom: false }));
+      } else {
+        setOverlay(prev => ({ top: true, bottom: true }));
+      }
+    };
+
+    target.addEventListener('scroll', scroll);
+    return () => target.removeEventListener('scroll', scroll);
+  }, []);
+
   const active = useCurrentMemberCount();
-  console.log(active);
+  console.log(active, overlay);
 
   return (
     <main className="flex max-h-[356px] min-h-full flex-col">
       <div className="w-full rounded bg-gray-100 p-2 font-semibold text-gray-800">
         {active} active users.
       </div>
-      <div className="h-[calc(100%-77px)] overflow-y-auto" ref={ref}>
-        <ul className="flex flex-col justify-end divide-y divide-gray-200 py-2">
-          {messages.map((message, index) => (
-            <li key={index}>
-              <div className="p-2">
-                <p className="text-xs font-medium text-gray-600">
-                  {message.sender}
-                </p>
-                <p className="font-medium">{message.message}</p>
-              </div>
-            </li>
-          ))}
-        </ul>
+      <div className="relative h-[calc(100%-77px)] overflow-hidden">
+        <div className="h-full overflow-y-auto" ref={ref}>
+          <ul className="flex flex-col justify-end divide-y divide-gray-200 py-2">
+            {/* {messages.map((message, index) => (
+              <li key={index}>
+                <div className="p-2">
+                  <p className="text-xs font-medium text-gray-600">
+                    {message.sender}
+                  </p>
+                  <p className="font-medium">{message.message}</p>
+                </div>
+              </li>
+            ))} */}
+            {new Array(20).fill(0).map((_, index) => (
+              <li key={index}>
+                <div className="p-2">
+                  <p className="text-xs font-medium text-gray-600">{index}</p>
+                  <p className="font-medium">{index}</p>
+                </div>
+              </li>
+            ))}
+          </ul>
+        </div>
+        <div
+          className={cn(
+            'pointer-events-none absolute bottom-0 h-full w-full touch-none border border-red-200',
+            overlay.top &&
+              'before:absolute before:top-0 before:h-10 before:w-full before:bg-red-200',
+            overlay.bottom &&
+              'after:absolute after:bottom-0 after:h-10 after:w-full after:bg-red-200'
+          )}
+        ></div>
       </div>
       <form
         onSubmit={e => {
