cffeb2b73b0782a70e7496df766ea1668c539547
Arik Chakma <arikchangma@gmail.com>
2023-02-17 17:58:01 +0600

    Add Pusher auth endpoints
    This adds two new endpoints to the API that are used to authenticate
    users and channels with Pusher.

---
+++ pages/api/pusher/auth-channel.ts
@@ -0,0 +1,23 @@
+import { pusherServerClient } from "@/lib/pusher";
+import { NextApiRequest, NextApiResponse } from "next";
+
+
+export default function pusherAuthEndpoint(
+  req: NextApiRequest,
+  res: NextApiResponse
+) {
+  const { channel_name, socket_id } = req.body;
+  const { user_id } = req.headers;
+
+  if (!user_id || typeof user_id !== "string") {
+    res.status(404).send("lol");
+    return;
+  }
+  const auth = pusherServerClient.authorizeChannel(socket_id, channel_name, {
+    user_id,
+    user_info: {
+      name: "oaiwmeroauwhero;aijhwer",
+    },
+  });
+  res.send(auth);
+}
---
+++ pages/api/pusher/auth-user.ts
@@ -0,0 +1,28 @@
+import type { NextApiRequest, NextApiResponse } from 'next';
+import { getSession } from 'next-auth/react';
+import { getServerSession } from '@/lib/auth';
+import { pusherServerClient } from '@/lib/pusher';
+
+export default async function handler(
+  req: NextApiRequest,
+  res: NextApiResponse
+) {
+  const { socket_id } = req.body;
+
+  // Check that the socket_id is present
+  if (!socket_id)
+    return res.status(400).send('Bad request. Missing socket_id.');
+
+  // Get user's details from the server
+  const session = await getServerSession(req, res);
+
+  // If the session doesn't exist or the user is not authenticated, return an error
+  if (!session) return res.status(401).send('Unauthorized');
+
+  const auth = pusherServerClient.authenticateUser(socket_id, {
+    id: session.user?.id!,
+    email: session.user?.email!,
+  });
+
+  res.send(auth);
+}
