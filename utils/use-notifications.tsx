17b96002ad14467a2c4d8b2ec779ce77735c8ddd
Arik Chakma <arikchangma@gmail.com>
2023-02-20 15:44:04 +0600

    feat: add notifications
    - Add notifications to the app
    - Add a new endpoint to the API
    - Add a new store to the app

--- utils/use-notifications.tsx
+++ utils/use-notifications.tsx
@@ -1,15 +1,16 @@
 import { useState } from 'react';
+import { NotificationsUnseenProps } from '@/types/project';
 import { useQuery } from '@tanstack/react-query';
 import { usePrivateSubscribeToEvent } from '@/lib/stores/private-pusher-store';
 import { fetcher } from './fetcher';
 
 export function useNotifications() {
-  const [notifications, setNotifications] = useState<any[]>(
-    []
-  );
+  const [notifications, setNotifications] = useState<
+    NotificationsUnseenProps[]
+  >([]);
   const [count, setCount] = useState(0);
 
-  const { refetch } = useQuery<any[]>(
+  const { refetch } = useQuery<NotificationsUnseenProps[]>(
     ['notifications'],
     async () => {
       return await fetcher('/api/notifications');
@@ -17,12 +18,12 @@ export function useNotifications() {
     {
       onSuccess: data => {
         setNotifications(data);
-        setCount(data.filter(n => !n.seen).length)
+        setCount(data.filter(n => !n.seen).length);
       },
     }
   );
 
-  usePrivateSubscribeToEvent<any>(
+  usePrivateSubscribeToEvent<NotificationsUnseenProps>(
     'new-project-invitations',
     invitation => {
       refetch();
