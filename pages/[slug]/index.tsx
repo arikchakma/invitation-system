b434a9b9af8c29512a614eb60901c2b72191c922
Arik Chakma <arikchangma@gmail.com>
2023-02-21 22:47:52 +0600

    Add chat to project page
    This is a breaking change, as the chat is now rendered in a different place.

--- pages/[slug]/index.tsx
+++ pages/[slug]/index.tsx
@@ -32,8 +32,9 @@ export default function ProjectPage() {
             isOwner ? 'grid-cols-2' : 'grid-cols-1'
           )}
         >
+          <ChatWrapper />
           {isOwner && (
-            <div className="-sm:row-start-2">
+            <div>
               <InviteUserForm />
               <div className="mt-10">
                 <UsersTable />
@@ -41,7 +42,6 @@ export default function ProjectPage() {
               </div>
             </div>
           )}
-          <ChatWrapper />
         </div>
 
         {status === 'error' && (
